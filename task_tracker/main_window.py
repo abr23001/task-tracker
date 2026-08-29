a4000148b53cd2cc99df867c5bc3a616a4311fe4
Abdullah Rashid <abdullahrasheed.rblx@gmail.com>
2026-08-29 01:28:49 -0400

    Icon added to Create Plan

--- task_tracker/main_window.py
+++ task_tracker/main_window.py
@@ -1,5 +1,6 @@
-from PySide6 import QtWidgets, QtCore
+from PySide6 import QtWidgets, QtGui, QtCore
 from .plan import Plan
+
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -7,80 +8,90 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.setWindowTitle("Spooky Tracer")
 
-        self.planLandingPage = QtWidgets.QWidget()
+        # --- State ---
+        self.plans = []          # List to hold created plans
+        self.selectedApps = []   # List to hold apps selected on the create page
+        self.app_rows = {}  # Dict to map app names to their row widgets
 
-        self.plans = []
-
-        # Create a container widget to hold the pages
+        # Widgets: (Menu / Stats)
         self.pageContainer = QtWidgets.QWidget()
-
-        # Create the pages and buttons of the main window
         self.menuPage = QtWidgets.QWidget()
         self.statsPage = QtWidgets.QWidget()
         self.menuButton = QtWidgets.QPushButton("Menu")
         self.statsButton = QtWidgets.QPushButton("Stats")
 
-
-        # Create buttons and pages for the menu page
+        # Widgets: menu page's inner pages (landing / create / choose)
+        self.planLandingPage = QtWidgets.QWidget()
         self.createPage = QtWidgets.QWidget()
         self.choosePage = QtWidgets.QWidget()
         self.createButton = QtWidgets.QPushButton("Create a Plan")
-        self.chooseButton = QtWidgets.QPushButton("Choose a Plan") # dropdown menu to choose a plan
+        self.chooseButton = QtWidgets.QPushButton("Choose a Plan")  # dropdown menu to choose a plan
         self.createBackButton = QtWidgets.QPushButton("Back")
         self.chooseBackButton = QtWidgets.QPushButton("Back")
 
-        # Create a stacked widget to hold the pages of the main window
+        # Widgets: "create a plan" form
+        self.planNameInput = QtWidgets.QLineEdit(placeholderText="Coding Project")
+        self.planDescription = QtWidgets.QTextEdit(placeholderText="Describe your plan")
+        self.iconButton = QtWidgets.QPushButton("Choose Icon")
+        self.iconPreview = QtWidgets.QLabel()
+        self.iconPreview.setFixedSize(64, 64)
+        self.createPlan = QtWidgets.QComboBox()
+        self.createPlan.addItem("Roblox")
+        self.createPlan.addItem("Brave")
+
+        self.appListContainer = QtWidgets.QWidget()
+        self.appListContainer.setLayout(QtWidgets.QVBoxLayout())
+        self.appListContainer.layout().addWidget(QtWidgets.QLabel("Selected Apps:"))
+
+        # Widgets: "choose a plan" form
+        self.choosePlan = QtWidgets.QComboBox()
+        self.choosePlan.addItem("Study")
+        self.choosePlan.addItem("Gaming")
+
+        # Stacks
         self.stackedWidget = QtWidgets.QStackedWidget()
         self.stackedWidget.addWidget(self.menuPage)
         self.stackedWidget.addWidget(self.statsPage)
 
-
-        # Create a stacked widget to hold the pages of the menu page
         self.planStackedWidget = QtWidgets.QStackedWidget()
+        self.planStackedWidget.addWidget(self.planLandingPage)
+        self.planStackedWidget.addWidget(self.createPage)
+        self.planStackedWidget.addWidget(self.choosePage)
 
-
-        # Set up the layout for the buttons of the main window
+        # Layout: top-level + central widget
         buttonRow = QtWidgets.QHBoxLayout()
         buttonRow.addWidget(self.menuButton)
         buttonRow.addWidget(self.statsButton)
 
-        # Set up the layout for the page container
         self.pageContainer.setLayout(QtWidgets.QVBoxLayout())
         self.pageContainer.layout().addLayout(buttonRow)
         self.pageContainer.layout().addWidget(self.stackedWidget)
-
-        # Set the page container as the central widget of the main window
         self.setCentralWidget(self.pageContainer)
 
-        self.planLandingPage.setLayout(QtWidgets.QVBoxLayout())    
-        self.planStackedWidget.addWidget(self.planLandingPage)
-        self.planStackedWidget.addWidget(self.createPage)
-        self.planStackedWidget.addWidget(self.choosePage)
-        self.planLandingPage.layout().addWidget(self.createButton)
-        self.planLandingPage.layout().addWidget(self.chooseButton)
-
-        # Set up the layout for the menu page
+        # Layout: menu page (just holds the plan sub-stack, centered)
         self.menuPage.setLayout(QtWidgets.QVBoxLayout())
         self.menuPage.layout().addStretch()
         self.menuPage.layout().addWidget(self.planStackedWidget)
         self.menuPage.layout().addStretch()
 
-        # Set up the layout for the create page
-        self.createPlan = QtWidgets.QComboBox()
-        self.createPlan.addItem("Roblox")
-        self.createPlan.addItem("Brave")
+        # Layout: plan landing page
+        self.planLandingPage.setLayout(QtWidgets.QVBoxLayout())
+        self.planLandingPage.layout().addWidget(self.createButton)
+        self.planLandingPage.layout().addWidget(self.chooseButton)
 
+        # Layout: create page
         self.createPage.setLayout(QtWidgets.QVBoxLayout())
         self.createPage.layout().addStretch()
         self.createPage.layout().addWidget(self.createBackButton)
+        self.createPage.layout().addWidget(self.iconButton)
+        self.createPage.layout().addWidget(self.iconPreview)
+        self.createPage.layout().addWidget(self.planNameInput)
+        self.createPage.layout().addWidget(self.planDescription)
         self.createPage.layout().addWidget(self.createPlan)
+        self.createPage.layout().addWidget(self.appListContainer)
         self.createPage.layout().addStretch()
 
-        # Set up the layout for the choose page
-        self.choosePlan = QtWidgets.QComboBox()
-        self.choosePlan.addItem("Study")
-        self.choosePlan.addItem("Gaming")
-
+        # Layout: choose page
         self.choosePage.setLayout(QtWidgets.QVBoxLayout())
         self.choosePage.layout().addStretch()
         self.choosePage.layout().addWidget(self.chooseBackButton)
@@ -88,31 +99,60 @@ class MainWindow(QtWidgets.QMainWindow):
         self.choosePage.layout().addWidget(self.choosePlan)
         self.choosePage.layout().addStretch()
 
-
-        # Connect the buttons to their respective functions
+        # Signal connections
         self.menuButton.clicked.connect(self.show_menu_page)
         self.statsButton.clicked.connect(self.show_stats_page)
         self.createButton.clicked.connect(self.show_create_page)
         self.chooseButton.clicked.connect(self.show_choose_page)
         self.createBackButton.clicked.connect(self.show_plan_landing_page)
         self.chooseBackButton.clicked.connect(self.show_plan_landing_page)
+        self.createPlan.textActivated.connect(self.add_selected_app)
+        self.iconButton.clicked.connect(self.choose_icon)
 
-
-
+    # Top-level nav handlers
     def show_menu_page(self):
         self.stackedWidget.setCurrentWidget(self.menuPage)
 
-
     def show_stats_page(self):
         self.stackedWidget.setCurrentWidget(self.statsPage)
-        
 
+    # Menu sub-page handlers
     def show_create_page(self):
         self.planStackedWidget.setCurrentWidget(self.createPage)
-        
 
     def show_choose_page(self):
         self.planStackedWidget.setCurrentWidget(self.choosePage)
 
     def show_plan_landing_page(self):
         self.planStackedWidget.setCurrentWidget(self.planLandingPage)
+
+    # App-list handlers (create page)
+    def add_selected_app(self, app_name):
+        if app_name not in self.selectedApps:
+            self.selectedApps.append(app_name)
+            rowWidget = QtWidgets.QWidget()
+            rowWidget.setLayout(QtWidgets.QHBoxLayout())
+            rowWidget.layout().addWidget(QtWidgets.QLabel(app_name))
+            removeButton = QtWidgets.QPushButton("x")
+            removeButton.clicked.connect(lambda checked, app=app_name: self.remove_selected_app(app))
+            rowWidget.layout().addWidget(removeButton)
+            self.appListContainer.layout().addWidget(rowWidget)
+            self.app_rows[app_name] = rowWidget
+
+    def remove_selected_app(self, app_name):
+        if app_name in self.selectedApps:
+            self.selectedApps.remove(app_name)
+            rowWidget = self.app_rows.pop(app_name, None)
+            if rowWidget:
+                self.appListContainer.layout().removeWidget(rowWidget)
+                rowWidget.setParent(None)
+                rowWidget.deleteLater()
+
+    def choose_icon(self):
+        path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open Image",
+                                                             "", "Image Files (*.png *.jpg *.bmp)")
+        if path:
+            self.selectedIconPath = path
+            pixmap = QtGui.QPixmap(self.selectedIconPath)
+            pixmap = pixmap.scaled(64, 64, QtCore.Qt.IgnoreAspectRatio, QtCore.Qt.SmoothTransformation)
+            self.iconPreview.setPixmap(pixmap)
